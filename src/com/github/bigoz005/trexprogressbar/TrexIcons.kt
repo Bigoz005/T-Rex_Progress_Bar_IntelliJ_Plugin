5fafc9d524b73fdc57524e1f657746c1843315c0
Michał Nawrot <32684265+Bigoz005@users.noreply.github.com>
2019-07-17 14:42:58 +0200

    Major update
    Need to fix background gifs

--- src/com/github/bigoz005/trexprogressbar/TrexIcons.kt
+++ src/com/github/bigoz005/trexprogressbar/TrexIcons.kt
@@ -4,9 +4,12 @@ import com.intellij.openapi.util.IconLoader
 import com.intellij.util.ui.JBUI
 import java.awt.Color
 
-val TREX_ICON by lazy(LazyThreadSafetyMode.NONE) { IconLoader.getIcon("/trex.png") }
-val TREXR_ICON by lazy(LazyThreadSafetyMode.NONE) { IconLoader.getIcon("/trex2.png") }
+val TREX_ICON by lazy(LazyThreadSafetyMode.NONE) { IconLoader.getIcon("/trexgif.gif") }
+val TREXR_ICON by lazy(LazyThreadSafetyMode.NONE) { IconLoader.getIcon("/trexgif2.gif") }
+
+val TREX_BG by lazy(LazyThreadSafetyMode.NONE) { IconLoader.getIcon("/bck.gif") }
+val TREXR_BG by lazy(LazyThreadSafetyMode.NONE) { IconLoader.getIcon("/bckr.gif") }
 
 val TREX_WIDTH by lazy(LazyThreadSafetyMode.NONE) { JBUI.scale(32) }
 
-val TREX_COLOR by lazy(LazyThreadSafetyMode.NONE) { Color(0xFFF176) }
+val TREX_BGCOLOR by lazy(LazyThreadSafetyMode.NONE) { Color(0xFFFFFF) }
--- src/com/github/bigoz005/trexprogressbar/TrexProgressBarUi.kt
+++ src/com/github/bigoz005/trexprogressbar/TrexProgressBarUi.kt
@@ -10,10 +10,11 @@ import java.awt.geom.RoundRectangle2D
 
 class TrexProgressBarUi : BasicProgressBarUI() {
     override fun getPreferredSize(c: JComponent?): Dimension {
-        return Dimension(super.getPreferredSize(c).width, JBUI.scale(20))
+        return Dimension(100, 24)
     }
 
     private var trexX = 0
+    private var trexY = 0
     private var velocity = 1
 
     private fun updateX(maxX: Int) {
@@ -29,13 +30,13 @@ class TrexProgressBarUi : BasicProgressBarUI() {
 
     private fun drawIcon(g2d: Graphics2D, c: JComponent) {
         val scaledIcon = if (velocity > 0) TREX_ICON else TREXR_ICON
-        scaledIcon.paintIcon(c, g2d, trexX, -JBUI.scale(6))
+        scaledIcon.paintIcon(c, g2d, trexX, trexY)
     }
 
     private fun drawBackground(g2d: Graphics2D, c: JComponent) {
-        val r = Rectangle(progressBar.size)
-        g2d.paint = TREX_COLOR
-        g2d.fill(RoundRectangle2D.Float(0f, 0f, r.width.toFloat(), r.height.toFloat(), 0f, 0f))
+        val scaledBck = if (velocity > 0) TREX_BG else TREXR_BG
+        g2d.setColor(progressBar.getForeground());
+        scaledBck.paintIcon(c, g2d, 0, 0)
     }
 
     override fun paint(g: Graphics, c: JComponent) {
